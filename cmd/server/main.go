38b0ca845a0c5f7da4c40ce860026151904a0431
mohsen1299@gmail.com <mohsen1299@gmail.com>
2023-06-24 08:03:15 +0330

    add connection command in main file

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,10 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+
+	"github.com/mohsen12999/go-rest-api/internal/db"
+)
 
 // Run - is going to be responsible for the instantiation and startup of our go application
 func Run() error {
 	fmt.Println("starting up our application")
+
+	db, err := db.NewDatabase()
+	if err != nil {
+		fmt.Println("failed to connect to the database")
+		return err
+	}
+
+	if err := db.Ping(context.Background()); err != nil {
+		return err
+	}
+
 	return nil
 }
 
