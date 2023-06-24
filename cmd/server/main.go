214cef9e7f16f5f32a8934ec1daab0a173d96155
mohsen1299@gmail.com <mohsen1299@gmail.com>
2023-06-24 08:34:01 +0330

    use migrate instead of ping in main file

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"context"
 	"fmt"
 
 	"github.com/mohsen12999/go-rest-api/internal/db"
@@ -17,7 +16,8 @@ func Run() error {
 		return err
 	}
 
-	if err := db.Ping(context.Background()); err != nil {
+	if err := db.MigrateDB(); err != nil {
+		fmt.Println("failed to migrate database")
 		return err
 	}
 
